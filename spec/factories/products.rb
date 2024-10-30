9c167c1d22e10609392e387ffe87561b3401af5d
nor-wtag <nowreen.rafa@welldev.io>
2024-10-30 14:03:54 +0600

    Removed unnecessary files

--- spec/factories/products.rb
+++
@@ -1,9 +0,0 @@
-# FactoryBot.define do
-#   factory :product do
-#     title { 'Test Product' }
-#     description { 'This is a test product description.' }
-#     price { 29.99 }
-#     stock { 10 }
-#     association :user
-#   end
-# end
