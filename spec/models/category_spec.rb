e7f01eb3ab21a05489dda58a07480a1708ad1842
nor-wtag <nowreen.rafa@welldev.io>
2024-10-30 10:14:09 +0600

    Removed unnecessary models

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -1,12 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe Category, type: :model do
+  let(:user) { User.create(email: "test@example.com", password: "password") }
+  let(:product) { Product.new(user: user, title: "Sample Title", description: "Sample Description", price: 100.0, stock: 5) }
+  let(:category) { Category.new(name: "Electronics", description: "All electronic items") }
+
   describe 'validations for name and description in Category model' do
     it { should validate_presence_of(:name) }
     it { should validate_presence_of(:description) }
   end
 
-  describe 'association with products in Category model' do
-    it { should have_and_belongs_to_many(:products) }
+  describe "Category has a many-to-many association with product" do
+    it "can be associated with a product" do 
+      category.products << product
+      expect(category.products).to include(product)
+    end
   end
 end
--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -1,46 +1,50 @@
 require 'rails_helper'
 
-
 RSpec.describe Product, type: :model do
   let(:user) { User.create(email: "test@example.com", password: "password") }
 
+  let(:product) { Product.new(user: user, title: "Sample Title", description: "Sample Description", price: 100.0, stock: 5) }
+
   describe 'validations for title, description, price, and stock attributes in Product model' do
-    it "validates presence of title" do
-      product = Product.new(user: user, description: "Sample Description", price: 100.0, stock: 5)
+    it { should validate_presence_of(:title) }
+    it { should validate_presence_of(:description) }
+    it { should validate_presence_of(:price) }
+    it { should validate_presence_of(:stock) }
+    it { should validate_numericality_of(:price).is_greater_than(0) }
+    it { should validate_numericality_of(:stock).is_greater_than_or_equal_to(0) }
+
+    it "is invalid without a title" do
       product.title = nil
       expect(product).to_not be_valid
       expect(product.errors[:title]).to include("can't be blank")
     end
 
-    it 'validates presence of description' do
-      product = Product.new(user: user, title: "Sample Title", price: 100.0, stock: 5)
+    it "is invalid without a description" do
       product.description = nil
       expect(product).to_not be_valid
       expect(product.errors[:description]).to include("can't be blank")
     end
 
-    it 'validates presence of price"' do
-      product = Product.new(user: user, title: "Sample Title", description: "Sample Description", stock: 5)
+    it "is invalid without a price" do
       product.price = nil
       expect(product).to_not be_valid
       expect(product.errors[:price]).to include("can't be blank")
     end
 
-    it "validates numericality of price" do
-      product = Product.new(user: user, title: "Sample Title", description: "Sample Description", price: 0, stock: 5)
+    it "is invalid with a price less than or equal to 0" do
+      product.price = 0
       expect(product).to_not be_valid
       expect(product.errors[:price]).to include("must be greater than 0")
     end
 
-    it "validates presence of stock" do
-      product = Product.new(user: user, title: "Sample Title", description: "Sample Description", price: 100.0)
+    it "is invalid without stock" do
       product.stock = nil
       expect(product).to_not be_valid
       expect(product.errors[:stock]).to include("can't be blank")
     end
 
-    it "validates numericality of stock" do
-      product = Product.new(user: user, title: "Sample Title", description: "Sample Description", price: 100.0, stock: -1)
+    it "is invalid with stock less than 0" do
+      product.stock = -1
       expect(product).to_not be_valid
       expect(product.errors[:stock]).to include("must be greater than or equal to 0")
     end
@@ -53,6 +57,13 @@ RSpec.describe Product, type: :model do
     it { should have_many(:cart_items) }
     it { should have_many(:carts).through(:cart_items) }
     it { should have_many(:orders).through(:order_items) }
-    it { should have_and_belongs_to_many(:categories) }
+  end
+
+  describe "Products association with category for many-to-many relationship" do
+    it "can be associated with a category" do
+      category = Category.create(name: "Electronics", description: "All electronic items")
+      product.categories << category
+      expect(product.categories).to include(category)
+    end
   end
 end
