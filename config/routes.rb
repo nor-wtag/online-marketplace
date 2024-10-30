0123299abf899490ab7e0a6cef98a4cb2b9ec1e8
nor-wtag <nowreen.rafa@welldev.io>
2024-10-30 10:36:45 +0600

    Changed routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,27 @@
 Rails.application.routes.draw do
   root 'users#index'
-  resources :users, only: [ :new, :create ] do
+
+  resources :users do
     collection do
-      get :sign_in
-      post :create_session
-      delete :destroy_session
+      get 'sign_in', to: 'users#sign_in', as: 'sign_in'
+      post 'create_session', to: 'users#create_session', as: 'create_session'
+      get 'destroy_session', to: 'users#destroy_session', as: 'destroy_session'
+    end
+    member do
+      get 'delete', to: 'users#delete', as: 'delete'  # Route for the delete confirmation page
     end
   end
-  get 'products', to: 'users#products'
+
+  resources :products do
+    member do
+      get 'delete', to: 'products#delete', as: 'delete'
+    end
+  end
+
+  # resources :products do
+  #   member do
+  #     get 'edit'
+  #     patch 'update'
+  #   end
+  # end
 end
