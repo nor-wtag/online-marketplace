da79e6c4016bf3a1964176453c0076ee710cc289
nor-wtag <nowreen.rafa@welldev.io>
2024-10-30 14:00:57 +0600

    Removed unnecessary files

--- spec/factories/products.rb
+++
@@ -1,9 +0,0 @@
-# FactoryBot.define do
-#   factory :product do
-#     title { "Sample Product" }
-#     description { "Sample description" }
-#     price { 9.99 }
-#     stock { 10 }
-#     association :user
-#   end
-# end
