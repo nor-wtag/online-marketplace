f391d655e69dbaacbcdc9a656e813412ddddcd87
nor-wtag <nowreen.rafa@welldev.io>
2024-10-30 14:24:56 +0600

    Removed unnecessary lines

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,14 +1,3 @@
 class ApplicationController < ActionController::Base
   allow_browser versions: :modern
-  helper_method :current_user
-
-  def current_user
-    @current_user ||= User.find_by(id: session[:user_id])
-  end
-
-  def authenticate_user!
-    unless current_user
-      redirect_to sign_in_users_path, alert: 'You need to sign in first.'
-    end
-  end
 end
