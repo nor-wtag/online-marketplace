c19f73a921086186583b763a5dc5f72e1f9dabc5
nor-wtag <nowreen.rafa@welldev.io>
2024-10-31 17:33:15 +0600

    Modified controllers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,24 @@
 class ApplicationController < ActionController::Base
-  allow_browser versions: :modern
+  before_action :authenticate_user!, except: [:index]
+  before_action :authenticate_user!, unless: :devise_controller?
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  def after_sign_up_path_for(resource)
+    products_path
+  end
+
+  def after_sign_in_path_for(resource)
+    products_path
+  end
+
+  def after_sign_out_path_for(resource_or_scope)
+    root_path
+  end
+  
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :phone, :role])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:username, :phone, :role])
+  end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,37 @@
 class UsersController < ApplicationController
+  layout 'index'
+
+  before_action :set_user, only: [ :edit, :update ]
+  before_action :authenticate_user!, except: [ :index ]
+
+  def index
+    @user = User.new
+  end
+
+  def edit
+  end
+
+  def update
+    if @user.update(user_params)
+      redirect_to products_path, notice: 'User updated successfully!'
+    else
+      render :edit
+    end
+  end
+
+  # def destroy
+  #   @user.destroy
+  #   flash[:notice] = 'User deleted successfully.'
+  #   redirect_to users_path
+  # end
+
+  private
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  def user_params
+    params.require(:user).permit(:username, :email, :phone, :role, :password, :password_confirmation)
+  end
 end
