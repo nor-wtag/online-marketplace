597deb73c33e5637c7af0eb21a5ee7c2464a5c85
nor-wtag <nowreen.rafa@welldev.io>
2024-11-03 18:12:01 +0600

    Added routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,36 @@
 Rails.application.routes.draw do
-  devise_for :users
-
+  # 
+  devise_for :users, controllers: {
+    registrations: 'registrations'
+  }
+  # devise_for :users
   devise_scope :user do
     get '/logout', to: 'devise/sessions#destroy', as: :logout
+    delete '/delete', to: 'devise/registrations#destroy', as: :delete_account
   end
 
+  # as :user do
+  #   get 'users/edit', to: 'users#edit', as: :edit_user_registration
+  #   patch 'users', to: 'users#update', as: :user_registration
+  # end
+  
   root 'users#index'
+
+  resources :users, only: [:index] 
+  
+  resources :products do
+    member do
+      get 'delete', to: 'products#delete', as: 'delete'
+    end
+  end
+
+  
+
+
+  # # devise_for :users, controllers: { registrations: 'users/registrations' }
+  # devise_for :users, controllers: { registrations: 'registrations' }
+  
+  # root 'users#index'
   # resources :users do
   #   collection do
   #     get 'sign_in', to: 'users#sign_in', as: 'sign_in'
@@ -18,14 +43,14 @@ Rails.application.routes.draw do
   # end
   # 
   # resources :users, only: [:index, :show, :edit, :update, :destroy]
-  resources :users, only: [:index, :edit, :update, :destroy] do
-    member do
-      get 'delete'
-    end
-  end
-  resources :products, only: [:index, :show] do
-    member do
-      get 'delete', to: 'products#delete', as: 'delete'
-    end
-  end
+  # resources :users, only: [:index, :edit, :update, :destroy] do
+  #   member do
+  #     get 'delete'
+  #   end
+  # end
+  # resources :products, only: [:index, :show] do
+  #   member do
+  #     get 'delete', to: 'products#delete', as: 'delete'
+  #   end
+  # end
 end
