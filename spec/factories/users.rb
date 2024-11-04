ef54c4ff70365bd4ddae40959749eefb551daee5
nor-wtag <nowreen.rafa@welldev.io>
2024-11-04 16:08:29 +0600

    Added factorybot for user

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,28 +1,10 @@
-# FactoryBot.define do
-#   factory :user do
-#     username { 'testuser' }
-#     email { 'test@example.com' }
-#     phone { '01712345678' }
-#     password { 'password' }
-#     role { 'buyer' }
-#   end
-
-#   factory :invalid_user, parent: :user do
-#     username { '' }
-#     email { 'invalid-email' }
-#     phone { 'not_a_number' }
-#     password { 'short' }
-#     role { '' }
-#   end
-# end
-# 
 FactoryBot.define do
-factory :user do
-  username { "testuser" }
-  email { "testuser@example.com" }
-  phone { "01712345678" }
-  role { "buyer" }
-  password { "password123" }
-  password_confirmation { "password123" }
-end
+  factory :user do
+    username { "testuser" }
+    email { "test@example.com" }
+    password { "password" }
+    password_confirmation { "password" }
+    role { :buyer }
+    phone { "01712345678" }
+  end
 end
