6917880c3e069f85902e6d6aa4e44ee2d76b7567
nor-wtag <nowreen.rafa@welldev.io>
2024-11-06 18:50:16 +0600

    Added models features

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -2,8 +2,8 @@ class Ability
   include CanCan::Ability
 
   def initialize(user)
-    return unless user.present? # Return if no user is signed in
-
+    return unless user.present?
+    
     case user.role
     when 'admin'
       can :manage, :all
@@ -14,27 +14,43 @@ class Ability
       can :update, Product, user_id: user.id
       can :destroy, Product, user_id: user.id
 
-      # Sellers can read reviews of products they own
+      can :read, Category
+
       can :read, Review, product: { user_id: user.id }
+      can [:read, :update, :destroy], User, id: user.id
+
+       can :read, Order, order_items: { product: { user_id: user.id } }
+      can :read, OrderItem, order: { order_items: { product: { user_id: user.id } } }
 
     when 'buyer'
       can :read, Product
       can :read, Category
 
-      # Buyers can create, read, update, and destroy their own reviews
       can :create, Review
-      can [:read, :update, :destroy], Review, user_id: user.id
+      can :read, Review
+      can [ :update, :destroy ], Review, user_id: user.id
 
-      # Additional cart permissions for buyers
       can :create, Cart
       can :read, Cart, user_id: user.id
       can :update, Cart, user_id: user.id
       can :destroy, Cart, user_id: user.id
-      
+
       can :create, CartItem, cart: { user_id: user.id }
       can :read, CartItem, cart: { user_id: user.id }
       can :update, CartItem, cart: { user_id: user.id }
       can :destroy, CartItem, cart: { user_id: user.id }
+
+      can :create, Order
+      can :read, Order, user_id: user.id
+      can :update, Order, user_id: user.id
+      can :cancel, Order, user_id: user.id
+
+      can :create, OrderItem
+      can :read, OrderItem, order: { user_id: user.id }
+      can :update, OrderItem, order: { user_id: user.id }
+      can :destroy, OrderItem, order: { user_id: user.id }
+
+      can [:read, :update, :destroy], User, id: user.id
 
     end
   end
--- app/models/cart_item.rb
+++ app/models/cart_item.rb
@@ -4,4 +4,13 @@ class CartItem < ApplicationRecord
   validates :quantity, presence: true, numericality: { only_integer: true, greater_than: 0 }
   validates :cart_id, presence: true
   validates :product_id, presence: true
+  validate :quantity_within_stock
+
+  private
+
+  def quantity_within_stock
+    if quantity.present? && product.present? && quantity > product.stock
+      errors.add(:quantity, 'Not enough product available in stock')
+    end
+  end
 end
