6aaee8e5126af11415f8f6e14f27cf28809f6b07
nor-wtag <nowreen.rafa@welldev.io>
2024-11-10 13:19:33 +0600

    Added i18 specifications

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::Base
-  allow_browser versions: :modern
+  before_action :set_locale
+
+  def set_locale
+    I18n.locale = params[:locale] || I18n.default_locale
+  end
+
+  def default_url_options
+    { locale: I18n.locale }
+  end
 end
