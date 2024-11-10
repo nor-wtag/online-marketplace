67aa120a31fd1fc87b645b65e7c59a22eb955527
nor-wtag <nowreen.rafa@welldev.io>
2024-11-10 13:14:31 +0600

    Modified user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
-require 'digest'
-
 class User < ApplicationRecord
   enum :role, { admin: 0, buyer: 1, seller: 2, rider: 3 }
+
+  phony_normalize :phone, default_country_code: 'BD'
 
   has_many :products, dependent: :destroy
   has_many :reviews, dependent: :destroy
@@ -10,25 +10,11 @@ class User < ApplicationRecord
 
   validates :username, presence: true, uniqueness: { case_sensitive: false }
   validates :email, presence: true, uniqueness: { case_sensitive: false }, format: { with: URI::MailTo::EMAIL_REGEXP }
-
-  validates :phone, presence: true
-  validates :phone, format: {
-    with: /\A(017|013|018|019|015)\d{8}\z/,
-    message: 'must start with 0 and contain exactly 11 digits'
-  }
-
+  validates :phone, phony_plausible: true, presence: true
   validates :password, presence: true, length: { minimum: 6 }
-  validates :role, presence: true, inclusion: { in: roles.keys, message: '%{value} is not a valid role' }
+  validates :role, presence: true, inclusion: { in: roles.keys }
 
-  before_save :hash_password
-
-  def hash_password
-    self.password = Digest::SHA256.hexdigest(password) if password.present?
-  end
-
-  def authenticate_the_login(plain_password)
-    Digest::SHA256.hexdigest(plain_password) == password
-  end
+  attr_accessor :password
 
   def admin?
     role == 'admin'
