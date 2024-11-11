3562a8ffd6f23c4ac3e659b4463faf9f678aca56
nor-wtag <nowreen.rafa@welldev.io>
2024-11-11 18:12:26 +0600

    Added routes

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,8 @@ Rails.application.routes.draw do
 
   devise_scope :user do
     get '/logout', to: 'devise/sessions#destroy', as: :logout
-    delete '/delete', to: 'devise/registrations#destroy', as: :delete_account
+    get 'users/delete', to: 'registrations#delete', as: :delete_account
+    delete 'users/destroy', to: 'registrations#destroy', as: :destroy_user
   end
 
   root 'users#index'
@@ -16,6 +17,11 @@ Rails.application.routes.draw do
   resources :products do
     member do
       get 'delete', to: 'products#delete', as: 'delete'
+    end
+    resources :reviews, only: [:index, :new, :create, :edit, :update, :destroy] do
+      member do
+        get 'delete', to: 'reviews#delete', as: 'delete'
+      end
     end
   end
 
@@ -31,7 +37,11 @@ Rails.application.routes.draw do
     end
   end
 
-  resources :cart_items, only: [ :create, :update, :destroy ]
+  resources :cart_items, only: [:create, :update, :destroy] do
+    member do
+      get 'delete', to: 'cart_items#delete', as: 'delete'
+    end
+  end
   resource :cart, only: [ :show ]
 
   resources :orders, only: [:index, :show, :create ] do
@@ -42,22 +52,8 @@ Rails.application.routes.draw do
     resources :order_items, only: [:show, :update] do
       member do
         patch :update_status
+        patch :assign_rider
       end
     end
   end
-
-  # resource :cart, only: [:show] do
-  #   resources :cart_items, only: [:create, :update, :destroy], shallow: true do
-  #     member do
-  #       get 'delete', to: 'cart_items#delete', as: 'delete'
-  #     end
-  #   end
-  # end
-
-  # resources :cart_items do
-  #   member do
-  #     get 'delete', to: 'cart_items#delete', as: 'delete'
-  #   end
-  # end
-  # resource :cart, only: [:show]
 end
