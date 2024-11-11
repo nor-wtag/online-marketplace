c4f0b7afd5cab53fa60ac6f2b1ecc990ac0de898
nor-wtag <nowreen.rafa@welldev.io>
2024-11-11 09:35:53 +0600

    Changed the spec for users

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -8,21 +8,6 @@ RSpec.describe UsersController, type: :controller do
   end
 
   describe "Redirecting from sign up, sign in and signout processes" do
-    context "when signing in" do
-      it "signs in the user and redirects to homepage" do
-        sign_in user
-        get :homepage
-        expect(response).to render_template(:homepage)
-      end
-
-      it "signs out the user and redirects to root path" do
-        sign_in user
-        sign_out user
-        get :homepage
-        expect(response).to redirect_to(new_user_session_path)
-      end
-    end
-
     it "redirects to homepage after sign up" do
       expect(controller.send(:after_sign_up_path_for, user)).to eq(homepage_path)
     end
