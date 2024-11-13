8519f49a30126db3f867ad3f10a7fe4db1a3b17c
nor-wtag <nowreen.rafa@welldev.io>
2024-11-13 11:39:23 +0600

    Added i18 to reviews

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -5,12 +5,12 @@ class ReviewsController < ApplicationController
   before_action :set_review, only: [:edit, :update, :destroy, :delete]
   
   def show
-    @reviews=Review.all
+    @reviews = Review.all
   end
 
   def new
     unless purchased_product?
-      redirect_to product_path(@product), alert: 'You can only review products you have purchased.'
+      redirect_to product_path(@product), alert: t('reviews.alerts.not_purchased')
       return
     end
 
@@ -19,7 +19,7 @@ class ReviewsController < ApplicationController
 
   def create
     unless purchased_product?
-      redirect_to product_path(@product), alert: 'You can only review products you have purchased.'
+      redirect_to product_path(@product), alert: t('reviews.alerts.not_purchased')
       return
     end
 
@@ -27,7 +27,7 @@ class ReviewsController < ApplicationController
     @review.user = current_user
 
     if @review.save
-      redirect_to product_path(@product), notice: 'Review was successfully created.'
+      redirect_to product_path(@product), notice: t('reviews.notices.created')
     else
       flash.now[:alert] = @review.errors.full_messages.join(', ')
       render :new
@@ -35,12 +35,20 @@ class ReviewsController < ApplicationController
   end
 
   def edit
-
+    unless current_user == @review.user
+      redirect_to product_path(@product), alert: t('reviews.alerts.not_owner_edit')
+    end
   end
 
   def update
+    if current_user != @review.user
+      flash[:alert] = t('reviews.alerts.not_owner_update')
+      redirect_to product_path(@product)
+      return
+    end
+
     if @review.update(review_params)
-      redirect_to product_path(@product), notice: 'Review was successfully updated.'
+      redirect_to product_path(@product), notice: t('reviews.notices.updated')
     else
       flash.now[:alert] = @review.errors.full_messages.join(', ')
       render :edit
@@ -51,9 +59,13 @@ class ReviewsController < ApplicationController
   end
 
   def destroy
-    @review = @product.reviews.find(params[:id])
-    @review.destroy
-    redirect_to product_path(@product), notice: 'Review was successfully deleted.'
+    if current_user == @review.user
+      @review.destroy
+      redirect_to product_path(@product), notice: t('reviews.notices.deleted')
+    else
+      flash[:alert] = t('reviews.alerts.not_owner_delete')
+      redirect_to product_path(@product)
+    end
   end
 
   private
@@ -67,7 +79,11 @@ class ReviewsController < ApplicationController
   end
 
   def purchased_product?
-    current_user.buyer? && @product.orders.where(user_id: current_user.id, status: 'completed').exists?
+    return false unless current_user.buyer?
+    @product.order_items.joins(:order).where(
+      orders: { user_id: current_user.id },
+      status: 'received'
+    ).exists?
   end
 
   def review_params
