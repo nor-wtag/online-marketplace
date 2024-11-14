4f8a6b2453f658c6084de9f5d9964c2e2988c938
nor-wtag <nowreen.rafa@welldev.io>
2024-11-14 12:57:35 +0600

    Added i18 paths

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :authenticate_user!, unless: :devise_controller?
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :set_locale
   require 'cancan'
 
   rescue_from CanCan::AccessDenied do |exception|
@@ -28,5 +29,15 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [ :username, :email, :phone, :role, :password, :password_confirmation ])
+  end
+
+  private
+
+  def set_locale
+    I18n.locale = params[:locale] || I18n.default_locale
+  end
+
+  def default_url_options
+    { locale: I18n.locale }
   end
 end
